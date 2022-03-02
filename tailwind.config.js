ff5a704b0d5321f1d7251b4b7b03874dc0bc49ec
kjartanhr <kjartan@kvm.group>
2022-03-02 21:52:34 +0000

    Codebase initialisation

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				prompt: 'Prompt',
+				sans: 'DM Sans'
+			}
+		},
+	},
+	plugins: [],
+}
